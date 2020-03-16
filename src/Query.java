ab4c806fe50a8856bd05dab7ff39353b2b39b41b
katsushika <marcoderio850@gmail.com>
2020-03-16 15:34:34 -0600

    Funcion JOIN y condicion de igualdad agregadas

--- src/Query.java
+++ src/Query.java
@@ -43,12 +43,73 @@ public class Query {
 
     // Operación FROM
     // Selecciona y retorna la tabla con el nombre especificado en el argumento
-    // Este es el primer paso en cualquier consulta
+    // Sirve para obtener únicamente una sola tabla
     public Table from(String dbname) {
         return db.getTable(dbname);
     }
 
-    // Operación Específica de WHERE
+    // Operación FROM con JOIN
+    // Recibe un par de tablas y retorna el producto cartesiano de éstas
+    // La condición del JOIN se establece en el WHERE, el cual se representa por la operación
+    // BETWEEN o por la operación EQUALS disponibles
+    // Los identificadores "id1" y "id2" son los nombres para reconocer la procedencia de cada
+    // atributo en la nueva tabla. El nombre  de cada atributo en la nueva tabla se 
+    // transforma en "idx.attribute_name"
+    public Table product(Table table1, String id1, Table table2, String id2) {
+        Table productTable = new Table();
+
+        for (int i = 0; i < table1.cardinality(); i++) {
+            // Obtiene la i-ésima tupla de la tabla 1
+            LinkedHashMap<String, String> tuple1 = table1.getTuple(i);
+            for (int j = 0; j < table2.cardinality(); j++) {
+                // Obtiene la j-ésima tupla de la tabla 2
+                LinkedHashMap<String, String> tuple2 = table2.getTuple(j);
+
+                // Combina ambas tuplas y las agrega a la nueva tabla
+                for (String attribute1 : table1.getAttributeNames()) {
+                    productTable.addValueTo(id1 + "." + attribute1, tuple1.get(attribute1));
+                }
+
+                for (String attribute2 : table2.getAttributeNames()) {
+                    productTable.addValueTo(id2 + "." + attribute2, tuple2.get(attribute2));
+                }
+            }
+        }
+
+        return productTable;
+    }
+
+    // Operación específica de WHERE: EQUALS
+    // "employees.department_id = departments.deparment_id"
+    // Seleciona todas las tupals de la tabla en el argumento en las que
+    // el identificador del departamento procedente de la tabla
+    // EMPLOYEES sea igual al identificador del deparamento procedente de la
+    // tabla DEPARTMENTS
+    public Table equals(Table table, String fromEmp_deptoID, String fromDeptos_deptoID) {
+        Table result = new Table();
+
+        // Encaso de que no se haya podido leer la tabla
+        if (table == null) {
+            return null;
+        }
+
+        // Itera sobre todas las tuplas de la tabla
+        for (int i = 0; i < table.cardinality(); i++) {
+            LinkedHashMap<String, String> tuple = table.getTuple(i);
+            // Compara ambos atributos
+            if (tuple.get(fromEmp_deptoID).trim() == tuple.get(fromDeptos_deptoID).trim()) {
+                for (String attribute : table.getAttributeNames()) {
+                    result.addValueTo(attribute, tuple.get(attribute));
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+
+    // Operación Específica de WHERE: BETWEEN
     // "salary BETWEEN minsalary AND maxsalary"
     // Selecciona todas las tuplas de la tabla en el argumento que contengan un salario
     // dentro de los límites especificados en los argumentos.
